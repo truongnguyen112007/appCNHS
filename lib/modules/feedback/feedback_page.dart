62b6f94c8a48960e1f46bf314b0940aa061f3b8d
DESKTOP-QCUULMR\Admin <locduoi1998>
2022-10-12 15:07:58 +0700

    [Feed detail]: update new flow.

---
+++ lib/modules/feedback/feedback_page.dart
@@ -0,0 +1,101 @@
+import 'package:base_bloc/components/app_scalford.dart';
+import 'package:base_bloc/modules/feedback/feedback_cubit.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/svg.dart';
+import '../../base/base_state.dart';
+import '../../components/app_button.dart';
+import '../../components/app_text.dart';
+import '../../components/app_text_field.dart';
+import '../../gen/assets.gen.dart';
+import '../../localizations/app_localazations.dart';
+import '../../theme/app_styles.dart';
+import '../../theme/colors.dart';
+
+class FeedbackPage extends StatefulWidget {
+  final int index;
+
+  const FeedbackPage({Key? key, required this.index}) : super(key: key);
+
+  @override
+  State<FeedbackPage> createState() => _FeedbackPageState();
+}
+
+class _FeedbackPageState extends BasePopState<FeedbackPage> {
+  final textController = TextEditingController();
+  late FeedBackCubit _bloc;
+
+  @override
+  void initState() {
+    _bloc = FeedBackCubit();
+    super.initState();
+  }
+
+  @override
+  Widget buildWidget(BuildContext context) {
+    return AppScaffold(
+      isTabToHideKeyBoard: true,
+      appbar: AppBar(
+          centerTitle: true,
+          leadingWidth: 35,
+          leading: Padding(
+            padding: EdgeInsets.only(left: 12.w),
+            child: InkWell(
+              onTap: () {
+                Navigator.pop(context);
+              },
+              child: SvgPicture.asset(Assets.svg.back),
+            ),
+          ),
+          backgroundColor: colorPrimaryOrange,
+          title: AppText(
+            AppLocalizations.of(context)!.textButtonComments,
+            style: typoSuperSmallTextRegular.copyWith(
+                color: colorWhite, fontSize: 16.sp),
+          )),
+      body: SizedBox(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+                padding: EdgeInsets.only(top: 33.h, left: 30.w, bottom: 17.h),
+                child: AppText(
+                  AppLocalizations.of(context)!.titleComment,
+                  style: typoExtraSmallTextBold.copyWith(
+                      color: colorPrimaryOrange),
+                )),
+            Padding(
+              padding: EdgeInsets.only(left: 30.w, right: 30.w),
+              child: AppTextField(
+                controller: textController,
+                maxLine: 10,
+                hintText: AppLocalizations.of(context)!.contentComment,
+              ),
+            ),
+            Center(
+              child: AppButton(
+                width: 300.w,
+                height: 50.h,
+                title: AppLocalizations.of(context)!.send,
+                textStyle: typoTitleHeader.copyWith(color: colorPrimaryOrange),
+                shapeBorder: RoundedRectangleBorder(
+                  side: const BorderSide(color: colorPrimaryOrange, width: 2),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                onPress: () {
+                  _bloc.sentOnClick(textController.text);
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  int get tabIndex => widget.index;
+}
---
+++ lib/modules/feedback/feedback_state.dart
@@ -0,0 +1,7 @@
+import 'package:equatable/equatable.dart';
+
+class FeedBackSate extends Equatable{
+  @override
+  List<Object?> get props =>[];
+
+}
